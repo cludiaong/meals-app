c5c63b8bc364e383116da6d7ba0b6ef80205dee7
claudiaong <s2200048@student.unklab.ac.id>
2021-12-01 16:34:01 +0900

    meals-app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer'; 
+import Categories from './components/Categories/Categories';
+import RandomMeal from './components/RandomMeal/RandomMeal';
+import HomePage from './components/HomePage/HomePage';
+import About from './components/About/index';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'; 
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/categories" component={Categories}/>
+        <Route exact path="/random" component={RandomMeal}/>
+        <Route exact path="/about" component={About}/>
+
+      </Switch>
+      <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useContext } from "react";
+import './styles.scss'
+
+export default function About() {
+    return (
+        <div>
+            <div class="container my-5">
+        <h1>About this Application</h1>
+        <p>Nama Lengkap : Anggreiny Claudia </p>
+        <p>NIM : 105022010011</p>
+        <p>Program Studi : Informatika</p>
+      </div>
+        </div>
+    )
+}
